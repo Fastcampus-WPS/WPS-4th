1f178a9f7b299ed0631158c68cb32fda13c77ecd
Lee HanYeong <dev@azelf.com>
2017-02-23 17:23:12 +0900

    Facebook - login_facebook기본구조 구현

--- Projects/django/facebook/django_app/member/urls.py
+++ Projects/django/facebook/django_app/member/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'member'
 urlpatterns = [
-    url(r'^login/', views.login_fbv, name='login'),
+    url(r'^login/$', views.login_fbv, name='login'),
+    url(r'^login/facebook/$', views.login_facebook, name='login_facebook'),
 ]
--- Projects/django/facebook/django_app/member/views.py
+++ Projects/django/facebook/django_app/member/views.py
@@ -1,8 +1,14 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
 def login_fbv(request):
+    facebook_app_id = settings.config['facebook']['app_id']
     context = {
-
+        'facebook_app_id': facebook_app_id,
     }
     return render(request, 'member/login.html', context)
+
+
+def login_facebook(request):
+    print(request.GET)
