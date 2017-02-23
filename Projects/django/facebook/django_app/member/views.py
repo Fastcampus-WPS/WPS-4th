fb6d8de7f3fd652ee3a01972329e41bbf2208ae4
Lee HanYeong <dev@azelf.com>
2017-02-23 17:54:14 +0900

    Facebook - 로그인 다이얼로그 이후 액세스토큰을 받고 검증구현

--- Projects/django/facebook/django_app/member/views.py
+++ Projects/django/facebook/django_app/member/views.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+import requests
 from django.shortcuts import render
 
 from facebook import settings
@@ -12,4 +15,41 @@ def login_fbv(request):
 
 
 def login_facebook(request):
-    print(request.GET)
+    APP_ID = settings.config['facebook']['app_id']
+    SECRET_CODE = settings.config['facebook']['secret_code']
+    REDIRECT_URI = 'http://localhost:8000/member/login/facebook/'
+    APP_ACCESS_TOKEN = '{app_id}|{secret_code}'.format(
+        app_id=APP_ID,
+        secret_code=SECRET_CODE
+    )
+
+    # login_fbv에서 페이스북 로그인으로 이동 후,
+    # 정상적인 로그인 후 (정상적으로 로그인 시 request.GET에 'code' parameter가 추가됨)
+    # redirect_uri를 이용해 다시 login_facebook으로 돌아온 후의 동작
+    if request.GET.get('code'):
+        code = request.GET.get('code')
+
+        # 전달받은 code값을 이용해서 access_token을 요청함
+        url_request_access_token = 'https://graph.facebook.com/v2.8/oauth/access_token'
+        params = {
+            'client_id': APP_ID,
+            'redirect_uri': REDIRECT_URI,
+            'client_secret': SECRET_CODE,
+            'code': code,
+        }
+        r = requests.get(url_request_access_token, params=params)
+        dict_access_token = r.json()
+        USER_ACCESS_TOKEN = dict_access_token['access_token']
+        print('ACCESS_TOKEN : %s' % USER_ACCESS_TOKEN)
+
+        # 유저 액세스 토큰과 앱 엑세스 토큰을 사용해서 토큰 검증을 거친다
+        url_debug_token = 'https://graph.facebook.com/debug_token'
+        params = {
+            'input_token': USER_ACCESS_TOKEN,
+            'access_token': APP_ACCESS_TOKEN,
+        }
+        r = requests.get(url_debug_token, params=params)
+        dict_debug_token = r.json()
+        pprint(dict_debug_token)
+        USER_ID = dict_debug_token['data']['user_id']
+        print('USER_ID : %s' % USER_ID)
