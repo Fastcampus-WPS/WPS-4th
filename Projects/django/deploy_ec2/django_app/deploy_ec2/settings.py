9cc05a1d4528ebfd3be87fd6fe04a58b5cd5ec27
Lee HanYeong <dev@azelf.com>
2017-03-08 11:46:07 +0900

    DB정보 파일에서 불러오도록 수정

--- Projects/django/deploy_ec2/django_app/deploy_ec2/settings.py
+++ Projects/django/deploy_ec2/django_app/deploy_ec2/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'deploy_ec2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config['db']['engine'],
+        'NAME': config['db']['name'],
+        'USER': config['db']['user'],
+        'PASSWORD': config['db']['password'],
+        'HOST': config['db']['host'],
+        'PORT': config['db']['port'],
     }
 }
 
