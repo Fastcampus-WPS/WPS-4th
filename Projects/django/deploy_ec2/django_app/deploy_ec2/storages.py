bae0d9b7d4eab2f1d903b4b1030c48ab1c26d9e1
Lee HanYeong <dev@azelf.com>
2017-03-09 14:58:52 +0900

    Static파일은 overwrite, Media파일은 중복 시 다른이름으로 저장하도록 함

--- Projects/django/deploy_ec2/django_app/deploy_ec2/storages.py
+++ Projects/django/deploy_ec2/django_app/deploy_ec2/storages.py
@@ -4,7 +4,9 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+    file_overwrite = True
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    file_overwrite = False
