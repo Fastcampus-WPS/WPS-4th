ba0145f4cdc9641de8bf61d673b25142bfe0f3c3
Lee HanYeong <dev@azelf.com>
2017-03-13 15:26:32 +0900

    InstagramAPI - post admin에 모델 추가

--- Projects/django/instagram_api/django_app/post/admin.py
+++ Projects/django/instagram_api/django_app/post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from post.models import Post
+from post.models import PostPhoto
+
+admin.site.register(Post)
+admin.site.register(PostPhoto)
