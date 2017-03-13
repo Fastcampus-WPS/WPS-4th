c6bbf6ae6648127ba7d5d2549fa3e0b2c8d9e58c
Lee HanYeong <dev@azelf.com>
2017-03-13 14:50:53 +0900

    InstagramAPI - Post, PostPhoto모델 추가 및 migration생성

--- Projects/django/instagram_api/django_app/post/models.py
+++ Projects/django/instagram_api/django_app/post/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PostPhoto(models.Model):
+    post = models.ForeignKey(Post)
+    photo = models.ImageField(upload_to='post')
