66b5e9af02db7cc3ce421ff4b4775d56acd51611
Lee HanYeong <dev@azelf.com>
2017-03-13 15:26:05 +0900

    InstagramAPI - db설정도 파일에서 가져오도록 함

--- Projects/django/instagram_api/django_app/config/settings.py
+++ Projects/django/instagram_api/django_app/config/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config['db']['engine'],
+        'NAME': config['db']['name'],
+        'USER': config['db']['user'],
+        'PASSWORD': config['db']['password'],
+        'HOST': config['db']['host'],
+        'PORT': config['db']['port'],
     }
 }
 
