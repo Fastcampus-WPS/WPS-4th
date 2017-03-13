19bffa942069b2e12df2d0555923bb2e24748e14
Lee HanYeong <dev@azelf.com>
2017-03-13 16:45:22 +0900

    InstagramAPI - post_photo_add구현

--- Projects/django/instagram_api/django_app/post/urls.py
+++ Projects/django/instagram_api/django_app/post/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from post import views
 
+app_name = 'post'
 urlpatterns = [
-    url(r'^post-create/$', views.post_create, name='create'),
+    url(r'^create/$', views.post_create, name='create'),
+    url(r'^photo/add/$', views.post_photo_add, name='photo_add'),
 ]
--- Projects/django/instagram_api/django_app/post/views.py
+++ Projects/django/instagram_api/django_app/post/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from post.models import Post
+from post.models import Post, PostPhoto
 
 User = get_user_model()
 
@@ -35,3 +35,37 @@ def post_create(request):
         return HttpResponse('{}'.format(post.pk))
     else:
         return HttpResponse('Post create view')
+
+
+@csrf_exempt
+def post_photo_add(request):
+    """
+    post_id, photo를 받아서
+    PostPhoto객체를 생성
+    이후 post_id와 post에 연결된 photo들의 id값을 리턴
+
+    extra:
+        post_id, photo가 request.POST, request.DATA에 존재하는지 예외처리
+        전달된 post_id에 해당하는 Post객체가 있는지 예외처리
+    """
+    if request.method == 'POST':
+        try:
+            post_id = request.POST['post_id']
+            photo = request.FILES['photo']
+            post = Post.objects.get(id=post_id)
+        except KeyError:
+            return HttpResponse('post_id and photo is required fields')
+        except Post.DoesNotExist:
+            return HttpResponse('post_id {} is not exist'.format(
+                request.POST['post_id']
+            ))
+        PostPhoto.objects.create(
+            post=post,
+            photo=photo
+        )
+        return HttpResponse('Post: {}, PhotoList: {}'.format(
+            post.id,
+            [photo.id for photo in post.postphoto_set.all()]
+        ))
+    else:
+        return HttpResponse('Post photo add view')
