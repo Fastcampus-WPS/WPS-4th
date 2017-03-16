7e00caa626c3033789c498aa7da210feb294b58e
Lee HanYeong <dev@azelf.com>
2017-03-16 20:27:58 +0900

    InstagramAPI - post models to_dict()메서드 삭제, nested serializer field구현

--- Projects/django/instagram_api/django_app/post/serializers/post.py
+++ Projects/django/instagram_api/django_app/post/serializers/post.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from post.models import Post
+from post.serializers.post_photo import PostPhotoSerializer
 
 __all__ = (
     'PostSerializer',
@@ -9,12 +10,14 @@ __all__ = (
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True)
+    postphoto_set = PostPhotoSerializer(many=True)
 
     class Meta:
         model = Post
         fields = (
             'author',
             'created_date',
+            'postphoto_set',
         )
         read_only_fields = (
             'created_date',
