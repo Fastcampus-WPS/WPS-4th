38cb3b3f43a0dcf168720c502ec621a2f4b7068e
Lee HanYeong <dev@azelf.com>
2017-03-16 15:14:50 +0900

    InstagramAPI - test_post_create코드 상세내용 구현

--- Projects/django/instagram_api/django_app/post/tests/tests_apis.py
+++ Projects/django/instagram_api/django_app/post/tests/tests_apis.py
@@ -20,7 +20,24 @@ class PostTest(APILiveServerTestCase):
         return user
 
     def test_post_create(self):
-        pass
+        # Post를 만들 유저를 생성 및 로그인
+        user = self.create_user()
+        self.client.login(
+            username=self.test_username,
+            password=self.test_password,
+        )
+        # Post를 생성하는 API주소를 reverse
+        url = reverse('post-create')
+        # Post를 생성하는 API주소에 POST요청, response를 받아옴
+        response = self.client.post(url)
+
+        # response의 status_code가 201(Created)이어야 함
+        self.assertEqual(response, status.HTTP_201_CREATED)
+        # 생성 후 Post인스턴스가 총 1개여야 함
+        self.assertEqual(Post.objects.count(), 1)
+        # 생성된 Post인스턴스의 author pk(id)가 테스트시 생성한 User의 pk(id)와 같아야 함
+        post = Post.objects.first()
+        self.assertEqual(post.author.id, user.id)
 
     def test_cannot_post_create_not_authenticated(self):
         url = reverse('post-create')
