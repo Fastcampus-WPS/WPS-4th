823a9270c65706bd97d11c4cf3b7181f9a0777cf
Lee HanYeong <dev@azelf.com>
2017-03-17 14:51:44 +0900

    InstagramAPI - PostPhoto모델 수정 및 migration생성

--- Projects/django/instagram_api/django_app/post/models.py
+++ Projects/django/instagram_api/django_app/post/models.py
@@ -6,10 +6,14 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     created_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-pk', )
+
 
 class PostPhoto(models.Model):
     post = models.ForeignKey(Post)
     photo = models.ImageField(upload_to='post')
+    created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         order_with_respect_to = 'post'
