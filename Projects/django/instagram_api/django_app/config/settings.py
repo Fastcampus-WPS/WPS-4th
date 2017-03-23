a77c192983b4212aef63d4289eebab9a882c8e2c
Lee HanYeong <dev@azelf.com>
2017-03-23 16:20:54 +0900

    InstagramDeploy - settings.py보완

--- Projects/django/instagram_api/django_app/config/settings.py
+++ Projects/django/instagram_api/django_app/config/settings.py
@@ -15,17 +15,14 @@ import json
 import os
 
 DEBUG = os.environ.get('MODE') == 'DEBUG'
+STORAGE_S3 = os.environ.get('STORAGE') == 'S3' or DEBUG is False
+DB_RDS = os.environ.get('DB') == 'RDS'
 
+# Paths
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
-
 # Template
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
-# Media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 # Static
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 BOWER_DIR = os.path.join(ROOT_DIR, 'bower_components')
@@ -33,10 +30,6 @@ STATICFILES_DIRS = (
     STATIC_DIR,
     BOWER_DIR,
 )
-STATIC_URL = '/static/'
-
-# Login redirect
-LOGIN_URL = '/admin/'
 
 # Config files
 CONF_DIR = os.path.join(ROOT_DIR, '.conf-secret')
@@ -47,7 +40,6 @@ else:
     CONFIG_FILE = os.path.join(CONF_DIR, 'settings_deploy.json')
 config_common = json.loads(open(CONFIG_FILE_COMMON).read())
 config = json.loads(open(CONFIG_FILE).read())
-
 # common과 현재 사용설정 (local또는 deploy)를 합쳐줌
 for key, key_dict in config_common.items():
     if not config.get(key):
@@ -55,6 +47,40 @@ for key, key_dict in config_common.items():
     for inner_key, inner_key_dict in key_dict.items():
         config[key][inner_key] = inner_key_dict
 
+# AWS
+AWS_ACCESS_KEY_ID = config['aws']['access_key_id']
+AWS_SECRET_ACCESS_KEY = config['aws']['secret_access_key']
+
+AWS_S3_HOST = 's3.{}.amazonaws.com'.format(config['aws']['s3_region'])
+AWS_S3_SIGNATURE_VERSION = config['aws']['s3_signature_version']
+AWS_STORAGE_BUCKET_NAME = config['aws']['s3_storage_bucket_name']
+AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
+
+# Static, Media storages
+if STORAGE_S3:
+    # Static files
+    STATICFILES_STORAGE = 'config.storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    STATIC_URL = 'https://{custom_domain}/{staticfiles_location}/'.format(
+        custom_domain=AWS_S3_CUSTOM_DOMAIN,
+        staticfiles_location=STATICFILES_LOCATION,
+    )
+    # Media files
+    DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+    MEDIA_URL = 'https://{custom_domain}/{mediafiles_location}/'.format(
+        custom_domain=AWS_S3_CUSTOM_DOMAIN,
+        mediafiles_location=MEDIAFILES_LOCATION
+    )
+else:
+    STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
+    STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
+
+
+# Login redirect
+LOGIN_URL = '/admin/'
 SECRET_KEY = config['django']['secret_key']
 ALLOWED_HOSTS = config['django']['allowed_hosts']
 REST_FRAMEWORK = {
