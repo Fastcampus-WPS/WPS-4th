d034584b632948164e39c637756e0ef8f0305a0f
Lee HanYeong <dev@azelf.com>
2017-04-06 15:34:19 +0900

    Front - Django설정폴더 이름 변경

--- Projects/deploy/docker-nginx-node-proxy/django_app/config/settings.py
+++ Projects/deploy/docker-nginx-node-proxy/django_app/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
