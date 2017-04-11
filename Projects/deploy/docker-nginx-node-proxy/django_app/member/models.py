4335ae4098e075e0bb7dde3052db7767e41dbe72
Lee HanYeong <dev@azelf.com>
2017-04-11 15:38:34 +0900

    Celery - CeleryTest model, admin, tasks구현

--- Projects/deploy/docker-nginx-node-proxy/django_app/member/models.py
+++ Projects/deploy/docker-nginx-node-proxy/django_app/member/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class CeleryTest(models.Model):
+    request_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ Projects/deploy/docker-nginx-node-proxy/django_app/member/tasks.py
@@ -0,0 +1,13 @@
+import time
+
+from django.utils import timezone
+
+from config.celery import app
+from member.models import CeleryTest
+
+
+@app.task
+def celery_test():
+    request_at = timezone.now()
+    time.sleep(10)
+    CeleryTest.objects.create(request_at=request_at)
